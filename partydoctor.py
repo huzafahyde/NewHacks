eb1af0b928b7814ead9d077bbfc3b246b31b3247
huzafahyde <35259367+huzafahyde@users.noreply.github.com>
2021-11-06 19:22:06 -0400

    added framwork

---
+++ partydoctor.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+leave = False
+show_school = False
+
+window = tk.Tk()
+window.geometry('600x500')
+window['background'] = '#E47B7B'
+welcome = tk.Label(
+    text="Welcome to Party Doctor"
+)
+welcome.pack()
+
+campus_question = tk.Label(
+    text="Are you on campus currently?"
+)
+campus_question.pack()
+
+frame = tk.Frame(master=window)
+
+
+def on_campus():
+    lb = tk.Listbox(window)
+    lb.insert(1, "McMaster University")
+    lb.insert(2, "Western University")
+    lb.pack()
+
+yes = tk.Button(master=frame,text="Yes", command = on_campus)
+yes.grid(column = 1,row=1)
+no = tk.Button(master=frame,text="No")
+no.grid(column = 2,row=1)
+
+frame.pack()
+window.mainloop()
+
