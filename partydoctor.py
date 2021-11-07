c2428feb80c0d49736ef0f3abb8ffd2fccd3f06b
huzafahyde <35259367+huzafahyde@users.noreply.github.com>
2021-11-06 20:21:37 -0400

    fjlks

--- partydoctor.py
+++ partydoctor.py
@@ -41,6 +41,8 @@ def choose_uni():
         print(lb.get(i))
     main_frame.destroy()
     yesno_frame.destroy()
+    show_guesses()
+
 
 
 yes = tk.Button(master=yesno_frame, text="Yes", command=on_campus)
@@ -51,4 +53,30 @@ no.grid(column=2, row=1)
 
 main_frame.pack()
 yesno_frame.pack()
+
+guess_frame = tk.Frame(master=window)
+
+def show_guesses():
+    guess_label = tk.Label(text="Which one of these is the person most likely suffering from")
+    guess_label.pack()
+    alc_poi = tk.Button(text='Alcohol Poisoning',master=guess_frame,command=show_response(1))
+    alc_poi.grid(column=1, row=1)
+    drug_over = tk.Button(text='Drug Overdose',master=guess_frame,command=show_response(2))
+    drug_over.grid(column=2, row=1)
+    choking = tk.Button(text='Choking',master=guess_frame,command=show_response(3))
+    choking.grid(column=3, row=1)
+    bleeding = tk.Button(text='Bleeding',master=guess_frame,command=show_response(4))
+    bleeding.grid(column=4, row=1)
+    other = tk.Button(text='Other or I do not know',master=guess_frame,command=show_response(5))
+    other.grid(column=5, row=1)
+
+    guess_frame.pack()
+
+responses = []
+
+def show_response(ID):
+
+
+
+
 window.mainloop()
